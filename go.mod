70497c0a44be7d57639f52f2115128d74c2efa6e
namelew <diogomaciel.cunha@gmail.com>
2023-06-21 11:31:52 -0300

    importando BTree e finalizando tabela hash com genéricos

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/namelew/DHashTable
 
-go 1.18
+go 1.20
+
+require (
+	github.com/tidwall/btree v1.6.0
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
+)
--- packages/hashtable/hashtable.go
+++ packages/hashtable/hashtable.go
@@ -1,10 +1,19 @@
 package hashtable
 
-type Table[I any, K any] struct {
-	size int
+import (
+	"errors"
+
+	"github.com/tidwall/btree"
+	"golang.org/x/exp/constraints"
+)
+
+type Table[I constraints.Ordered, K any] struct {
+	size  int
+	slots []btree.Map[I, K]
 }
 
-type Key[K any] interface {
+type Key[K constraints.Ordered] interface {
+	Value() K
 	Index() int
 }
 
@@ -12,19 +21,26 @@ func (t *Table[I, K]) hash(id Key[I]) int {
 	return id.Index() % t.size
 }
 
-func (t *Table[I, K]) Insert(id Key[I], data K) error {
-	return nil
+func (t *Table[I, K]) Write(id Key[I], data K) {
+	t.slots[t.hash(id)].Set(id.Value(), data)
 }
 
-func (t *Table[I, K]) Remove(id Key[I]) error {
+func (t *Table[I, K]) Delete(id Key[I]) error {
+	_, notFound := t.slots[t.hash(id)].Delete(id.Value())
+
+	if notFound {
+		return errors.New("delete action failed: unable to find register")
+	}
+
 	return nil
 }
 
 func (t *Table[I, K]) Get(id Key[I]) (K, error) {
-	var result K
-	return result, nil
-}
+	result, notFound := t.slots[t.hash(id)].Get(id.Value())
 
-func (t *Table[I, K]) Update(id Key[I], new K) error {
-	return nil
+	if notFound {
+		return result, errors.New("query action failed: unable to find register")
+	}
+
+	return result, nil
 }
