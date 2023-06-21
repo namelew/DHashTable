563e1ef90d98c748fc16cfa856630eea5c0b0d68
namelew <diogomaciel.cunha@gmail.com>
2023-06-21 11:56:06 -0300

    Criando interface para HashTables e separando os tipos Open e Linked

--- packages/hashtable/hashtable.go
+++ packages/hashtable/hashtable.go
@@ -7,25 +7,38 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-type Table[I constraints.Ordered, K any] struct {
-	size  int
-	slots []btree.Map[I, K]
-}
-
 type Key[K constraints.Ordered] interface {
 	Value() K
 	Index() int
 }
 
-func (t *Table[I, K]) hash(id Key[I]) int {
+type HashTable[I constraints.Ordered, K any] interface {
+	hash(id Key[I]) int
+	Insert(id Key[I], data K) error
+	Delete(id Key[I]) error
+	Search(id Key[I]) (K, error)
+}
+
+type Linked[I constraints.Ordered, K any] struct {
+	size  int
+	slots []btree.Map[I, K]
+}
+
+type Open[I constraints.Ordered, K any] struct {
+	size  int
+	slots []K
+}
+
+func (t *Linked[I, K]) hash(id Key[I]) int {
 	return id.Index() % t.size
 }
 
-func (t *Table[I, K]) Write(id Key[I], data K) {
+func (t *Linked[I, K]) Insert(id Key[I], data K) error {
 	t.slots[t.hash(id)].Set(id.Value(), data)
+	return nil
 }
 
-func (t *Table[I, K]) Delete(id Key[I]) error {
+func (t *Linked[I, K]) Delete(id Key[I]) error {
 	_, notFound := t.slots[t.hash(id)].Delete(id.Value())
 
 	if notFound {
@@ -35,12 +48,29 @@ func (t *Table[I, K]) Delete(id Key[I]) error {
 	return nil
 }
 
-func (t *Table[I, K]) Get(id Key[I]) (K, error) {
+func (t *Linked[I, K]) Search(id Key[I]) (K, error) {
 	result, notFound := t.slots[t.hash(id)].Get(id.Value())
 
 	if notFound {
 		return result, errors.New("query action failed: unable to find register")
 	}
 
+	return result, nil
+}
+
+func (t *Open[I, K]) hash(id Key[I]) int {
+	return id.Index() % t.size
+}
+
+func (t *Open[I, K]) Insert(id Key[I], data K) error {
+	return nil
+}
+
+func (t *Open[I, K]) Delete(id Key[I]) error {
+	return nil
+}
+
+func (t *Open[I, K]) Search(id Key[I]) (K, error) {
+	var result K
 	return result, nil
 }
